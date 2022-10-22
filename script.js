d9934c319cb64103bba1ec2a4c6995b73ae89f3a
alejandrosatxc <alejandro@satxconsultants.com>
2022-10-22 00:37:21 -0500

    MVP

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 const btn = document.querySelector("#generate-btn")
 
 const generatePassword = (length) => {
+    //enfore max password length at 128 characters
+    if(length > 128) length = 128
+
+    //check values of options checkboxes
+    var upper = document.querySelector('#upper').checked
+    var lower = document.querySelector('#lower').checked
+    var numbers = document.querySelector('#numbers').checked
+    var special = document.querySelector('#special').checked
+
     var result = ''
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+'
+    var characters = ''
+
+    //For each option that is checked, include that set of characters as part of the password
+    if (upper)   characters += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    if (lower)   characters += 'abcdefghijklmnopqrstuvwxyz'
+    if (numbers) characters += '0123456789'
+    if (special) characters += '!@#$%^&*()-_+'
+
+    //Generate random password using characters string built from checkbox options
     for(var i = 0; i < length; i++) {
         result += characters.charAt(Math.floor(Math.random() * characters.length))
     }
@@ -10,7 +27,8 @@ const generatePassword = (length) => {
 }
 
 const myfunc = () => {
-    const password = generatePassword(8)
+    const length = document.querySelector("#length").value
+    const password = generatePassword(length)
     console.log(password)
     const pwtextfield = document.querySelector('#pwresult')
     pwtextfield.innerHTML = password
